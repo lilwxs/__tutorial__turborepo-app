83da95e1c16ed48a22c2514896eaa2ab0055aba4
Hoàng Thái Ninh <124787909+lilwxs@users.noreply.github.com>
2023-12-29 13:44:12 +0700

    Update eslint-web.js

--- packages/eslint-config/eslint-web.js
+++ packages/eslint-config/eslint-web.js
@@ -26,14 +26,29 @@ module.exports = {
   },
 
   rules: {
+    "react/jsx-key": "off", // Bỏ qua kiểm tra key trong jsx
+    "react/jsx-sort-props": "off", // Bỏ qua kiểm tra props sort
+    "react/jsx-no-undef": "off", // Bỏ qua kiểm tra jsx undef
+    "react/jsx-no-useless-fragment": "off", // Bỏ qua kiểm tra jsx fragment
+    "react/jsx-uses-react": "off", // Bỏ qua kiểm tra jsx uses react
+    "react/jsx-fragments": "off", // Bỏ qua kiểm tra jsx fragments
+    "react/jsx-props-no-spreading": "off", // Bỏ qua kiểm tra không sử dụng spread props
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }], // Kiểm tra đuôi file jsx
+    "react/function-component-definition": "off", // Bỏ qua kiểm tra function component definition
     "import/no-default-export": "off", // Bỏ qua kiểm tra export default
     "react/prop-types": "off", // Bỏ qua kiểm tra props type
-    "react/jsx-key": "off", // Bỏ qua kiểm tra key trong jsx
     "import/prefer-default-export˘": "off", // Bỏ qua kiểm tra export default
     "import/prefer-default-export": "warn", // Cảnh báo khi không sử dụng export default
     "no-unneeded-ternary": "off", // Bỏ qua kiểm tra không cần thiết ternary
-    "react/jsx-props-no-spreading": "off", // Bỏ qua kiểm tra không sử dụng spread props
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }], // Kiểm tra đuôi file jsx
+    "unicorn/filename-case": [
+      "error",
+      {
+        cases: {
+          camelCase: true,
+          kebabCase: true,
+        },
+      },
+    ],
     "react-hooks/rules-of-hooks": "error", // Kiểm tra hook rules
     "react-hooks/exhaustive-deps": "warn", // Kiểm tra hook deps
   },
